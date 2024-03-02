a1caba84366958c4b31d3fd16e11e35db4ab39ae
enriquemacias21 <enriquemacias21@hotmail.com>
2024-03-01 22:24:27 -0500

    .\holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+def bienvenido_mensaje():
+    name= input("Ingrese su nombre: ")
+    print("Bienvenido, ", name, " al cruso Dejango y React")
+
+if __name__== "__main__":
+    bienvenido_mensaje()
